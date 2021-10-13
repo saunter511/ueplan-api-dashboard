4e17cd97e67091ecc07da298dca7e42f43967c20
saunter511 <blazej.barcicki@relayr.io>
2021-10-13 12:16:07 +0200

    feature: improved loaders

--- src/components/molecules/loading/index.tsx
+++ src/components/molecules/loading/index.tsx
@@ -1,21 +1,10 @@
 import { FC } from "react";
-import { motion } from "framer-motion";
+import HashLoader from "react-spinners/HashLoader";
 
 const Loading: FC = () => {
   return (
     <div className="flex h-screen justify-center items-center">
-      <div className="bg-base-300 w-96 h-2">
-        <motion.div
-          className="w-32 bg-accent h-2"
-          animate={{ x: "16rem" }}
-          transition={{
-            duration: 2,
-            times: [0, 0.2, 1],
-            repeat: Infinity,
-            repeatType: "reverse",
-          }}
-        />
-      </div>
+      <HashLoader color={"#000"} loading size={50} />
     </div>
   );
 };
