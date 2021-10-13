80d2066e2a9b1859ff48f805eebd3bc78d4cda15
saunter511 <blazej.barcicki@relayr.io>
2021-10-13 15:43:58 +0200

    feature: daily stats

--- src/components/pages/home/Stats.tsx
+++ src/components/pages/home/Stats.tsx
@@ -1,13 +1,17 @@
 import { FC } from "react";
 
+import { BsPerson, BsPersonBadge } from "react-icons/bs";
+
 interface IStatsProps {
   total: number;
   unique: number;
+  daily: any;
 }
 
-export const Stats: FC<IStatsProps> = ({ total, unique }) => {
+export const Stats: FC<IStatsProps> = ({ total, unique, daily }) => {
+  console.log(daily);
   return (
-    <div className=" flex xl:flex-nowrap sm:w-full flex-wrap shadow stats">
+    <div className=" flex xl:flex-nowrap sm:w-full sm:grid-flow-row flex-wrap shadow stats">
       <div className="stat">
         <div className="stat-figure text-primary">
           <svg
@@ -45,6 +49,42 @@ export const Stats: FC<IStatsProps> = ({ total, unique }) => {
         </div>
         <div className="stat-title">Unikalni użytkownicy</div>
         <div className="stat-value text-info">{unique}</div>
+      </div>
+      <div className="stat">
+        <div className="stat-figure text-warning">
+          <BsPerson className="inline-block w-8 h-8 stroke-current" />
+        </div>
+        <div className="stat-title">Użytkownicy dziś</div>
+        <div className="stat-value text-warning">{daily.today.length}</div>
+        {daily.yesterday.length - daily.today.length < 0 ? (
+          <div className="stat-desc text-success">
+            ↗︎ {daily.yesterday.length - daily.today.length} więcej niż wczoraj
+          </div>
+        ) : (
+          <div className="stat-desc text-error">
+            ↘︎ {daily.yesterday.length - daily.today.length} mniej niż wczoraj
+          </div>
+        )}
+      </div>
+      <div className="stat">
+        <div className="stat-figure text-secondary">
+          <BsPersonBadge className="inline-block w-8 h-8 stroke-current" />
+        </div>
+        <div className="stat-title">Unikalni użytkownicy dziś</div>
+        <div className="stat-value text-secondary ">
+          {daily.todayUnique.length}
+        </div>
+        {daily.yesterdayUnique.length - daily.todayUnique.length < 0 ? (
+          <div className="stat-desc text-success">
+            ↗︎ {daily.yesterdayUnique.length - daily.todayUnique.length} więcej
+            niż wczoraj
+          </div>
+        ) : (
+          <div className="stat-desc text-error">
+            ↘︎ {daily.yesterdayUnique.length - daily.todayUnique.length} mniej
+            niż wczoraj
+          </div>
+        )}
       </div>
     </div>
   );
